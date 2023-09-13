fbe2dc35c87022211ee2cb909492956749d01593
Adham Ayad <aboayad557@gmail.com>
2023-09-13 17:30:44 +0300

    generate file on hive to enable me to save object

--- lib/models/note_model.dart
+++ lib/models/note_model.dart
@@ -1,7 +1,22 @@
-class NoteModel {
+import 'package:hive/hive.dart';
+
+part 'note_model.g.dart'; //! important to be the same file name 'note_model' and save
+//! after this run 'flutter packages pub run build_runner build' in terminal to enable you to save object with this type
+
+@HiveType(typeId: 0) //! type id for class cant repeat it for another class
+class NoteModel extends HiveObject{ //? can reapet it into class
+
+//! make id for every feild 'variables'
+  @HiveField(0)
   final String title;
+
+  @HiveField(1)
   final String subTitle;
+
+  @HiveField(2)
   final String date;
+
+  @HiveField(3)
   final int color;
 
   NoteModel(
