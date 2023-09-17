1ec83f3e528afa438e9c133aa823d19b9146fc28
Adham Ayad <aboayad557@gmail.com>
2023-09-17 22:25:28 +0300

    Add Note State

--- lib/cubits/add_note_cubit/add_note_state.dart
+++ lib/cubits/add_note_cubit/add_note_state.dart
@@ -1,6 +1,12 @@
 part of 'add_note_cubit.dart';
 
 @immutable
-sealed class AddNoteState {}
+abstract class AddNoteState {} // todo: class that we use in cubit and all classes extends from it
 
-final class AddNoteInitial extends AddNoteState {}
+final class AddNoteInitial extends AddNoteState {} //? intial state
+final class AddNoteLoading extends AddNoteState {} //? whenn loading state
+final class AddNoteSuccess extends AddNoteState {} //? when it is success
+final class AddNoteFailure extends AddNoteState {//? when go error
+  final String errMessage;//* to take message error with state
+  AddNoteFailure(this.errMessage); //? constructor
+} 
