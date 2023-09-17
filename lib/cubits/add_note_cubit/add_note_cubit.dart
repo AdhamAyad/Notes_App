5ec762bd73c55f8554a6300b184c7d0f227cfb23
Adham Ayad <aboayad557@gmail.com>
2023-09-17 22:57:45 +0300

    add note method to add note cubit

--- lib/cubits/add_note_cubit/add_note_cubit.dart
+++ lib/cubits/add_note_cubit/add_note_cubit.dart
@@ -1,8 +1,25 @@
 import 'package:bloc/bloc.dart';
+import 'package:hive/hive.dart';
 import 'package:meta/meta.dart';
+import 'package:note_sapp/models/note_model.dart';
+import 'package:note_sapp/widgets/constants.dart';
 
 part 'add_note_state.dart';
 
 class AddNoteCubit extends Cubit<AddNoteState> {
   AddNoteCubit() : super(AddNoteInitial());
+
+  addNote(NoteModel note) async{
+    emit(AddNoteLoading()); //? when call the method yhe state will be loading while adding data
+    try{
+    var notesBox = Hive.box<NoteModel>(kNotesBox); //! Hive.box<NoteModel>(kNotesBox) → take the box that we will store in it as note model
+    await notesBox.add(note); //! await notesBox.add(note) → thats store the object into box
+    emit(AddNoteSuccess()); //? after adding the object change sate to success 
+    }catch(e)
+    {
+      emit(AddNoteFailure(e.toString())); //? if have error change state and send the message of error
+    }
+    
+
+  }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/adapters.dart';
+import 'package:note_sapp/models/note_model.dart';
 import 'package:note_sapp/views/notes_view.dart';
 import 'package:note_sapp/widgets/constants.dart';
 
@@ -7,6 +8,7 @@ void main() async{
 
   await Hive.initFlutter(); //! make it to start Hive
   await Hive.openBox(kNotesBox); //! box that we will store inside
+  Hive.registerAdapter(NoteModelAdapter()); //! to start stor as this model
 
   runApp(const NotesApp());
 }
